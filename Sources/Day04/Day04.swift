f10fcea0ebaf40a45a8546b04715f2c9e046e517
Gereon Steffens <gereon@steffens.org>
2022-10-04 19:54:55 +0200

    day 4

--- Sources/Day04/Day04.swift
+++ Sources/Day04/Day04.swift
@@ -5,18 +5,112 @@
 //
 
 import AoCTools
+import Foundation
+
+private struct GuardRecord {
+    let id: Int
+    let date: Date
+    let asleep: [Bool]
+
+    var minutesAsleep: Int {
+        asleep.count { $0 }
+    }
+}
 
 final class Day04: AOCDay {
-    let input: String
+    private var records = [GuardRecord]()
+    let input: [String]
+
     init(rawInput: String? = nil) {
-        self.input = rawInput ?? Self.rawInput
+        let input = rawInput ?? Self.rawInput
+        self.input = input.lines
+    }
+
+    private func parseInput() {
+        guard records.isEmpty else { return }
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "[yyyy-MM-dd HH:mm]"
+        let calendar = Calendar.current
+
+        var byDate = [Date: [String]]()
+        for line in input {
+            let parts = line.components(separatedBy: " ")
+            // moving the date to "modern" times avoid some really weird (leap-second?) results
+            let fixedP0 = parts[0].replacingOccurrences(of: "1518", with: "1918")
+            let lineDate = dateFormatter.date(from: "\(fixedP0) \(parts[1])")!
+            byDate[lineDate] = parts
+        }
+
+        var currentGuard: Int?
+        var fallsAsleep = -1
+        var recordDate = Date()
+        var asleep = [Bool](repeating: false, count: 60)
+
+        var records = [GuardRecord]()
+
+        for (date, parts) in byDate.sorted(by: { $0.key < $1.key }) {
+            let minute = calendar.component(.minute, from: date)
+            switch parts[2] {
+            case "Guard":
+                if let current = currentGuard {
+                    let record = GuardRecord(id: current, date: calendar.startOfDay(for: recordDate), asleep: asleep)
+                    records.append(record)
+                    asleep = [Bool](repeating: false, count: 60)
+                }
+                currentGuard = Int(parts[3].dropFirst())
+            case "falls":
+                fallsAsleep = minute
+                recordDate = date
+            case "wakes":
+                for s in fallsAsleep..<minute {
+                    asleep[s] = true
+                }
+                fallsAsleep = -1
+            default: fatalError()
+            }
+        }
+
+        if let current = currentGuard {
+            let record = GuardRecord(id: current, date: calendar.startOfDay(for: recordDate), asleep: asleep)
+            records.append(record)
+        }
+
+        self.records = records
     }
 
     func part1() -> Int {
-        return 0
+        parseInput()
+        var sleepPerGuard = [Int: Int]()
+        for record in records {
+            sleepPerGuard[record.id, default: 0] += record.minutesAsleep
+        }
+
+        let sleepiest = sleepPerGuard.max { $0.value < $1.value }!
+
+        var sleepiestMinute = [Int: Int]()
+        for record in records where record.id == sleepiest.key {
+            for (minute, sleep) in record.asleep.enumerated() {
+                sleepiestMinute[minute, default: 0] += sleep ? 1 : 0
+            }
+        }
+
+        let minute = sleepiestMinute.max { $0.value < $1.value }!
+
+        return sleepiest.key * minute.key
     }
 
     func part2() -> Int {
-        return 0
+        parseInput()
+        var sleepiestMinute = [Pair<Int, Int>: Int]()
+
+        for record in records {
+            for (minute, sleep) in record.asleep.enumerated() {
+                sleepiestMinute[Pair(record.id, minute), default: 0] += sleep ? 1 : 0
+            }
+        }
+
+        let sleepiest = sleepiestMinute.max { $0.value < $1.value }!
+
+        return sleepiest.key.first * sleepiest.key.second
     }
 }
--- Tests/Day04Tests.swift
+++ Tests/Day04Tests.swift
@@ -3,12 +3,48 @@ import XCTest
 
 final class Day04Tests: XCTestCase {
     func testDay04_1() throws {
-        let day = Day04(rawInput: "foo")
-        XCTAssertEqual(day.part1(), 0)
+        let day = Day04(rawInput: """
+            [1918-11-01 00:00] Guard #10 begins shift
+            [1918-11-01 00:05] falls asleep
+            [1918-11-01 00:25] wakes up
+            [1918-11-01 00:30] falls asleep
+            [1918-11-01 00:55] wakes up
+            [1918-11-01 23:58] Guard #99 begins shift
+            [1918-11-02 00:40] falls asleep
+            [1918-11-02 00:50] wakes up
+            [1918-11-03 00:05] Guard #10 begins shift
+            [1918-11-03 00:24] falls asleep
+            [1918-11-03 00:29] wakes up
+            [1918-11-04 00:02] Guard #99 begins shift
+            [1918-11-04 00:36] falls asleep
+            [1918-11-04 00:46] wakes up
+            [1918-11-05 00:03] Guard #99 begins shift
+            [1918-11-05 00:45] falls asleep
+            [1918-11-05 00:55] wakes up
+            """)
+        XCTAssertEqual(day.part1(), 240)
     }
 
     func testDay04_2() throws {
-        let day = Day04(rawInput: "bar")
-        XCTAssertEqual(day.part2(), 0)
+        let day = Day04(rawInput: """
+            [1918-11-01 00:00] Guard #10 begins shift
+            [1918-11-01 00:05] falls asleep
+            [1918-11-01 00:25] wakes up
+            [1918-11-01 00:30] falls asleep
+            [1918-11-01 00:55] wakes up
+            [1918-11-01 23:58] Guard #99 begins shift
+            [1918-11-02 00:40] falls asleep
+            [1918-11-02 00:50] wakes up
+            [1918-11-03 00:05] Guard #10 begins shift
+            [1918-11-03 00:24] falls asleep
+            [1918-11-03 00:29] wakes up
+            [1918-11-04 00:02] Guard #99 begins shift
+            [1918-11-04 00:36] falls asleep
+            [1918-11-04 00:46] wakes up
+            [1918-11-05 00:03] Guard #99 begins shift
+            [1918-11-05 00:45] falls asleep
+            [1918-11-05 00:55] wakes up
+            """)
+        XCTAssertEqual(day.part2(), 4455)
     }
 }
