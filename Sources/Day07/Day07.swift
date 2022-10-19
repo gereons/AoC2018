9bc0490da514c70a89de089a48abb99f95345f90
Gereon Steffens <gereon@steffens.org>
2022-10-19 09:26:30 +0200

    day 7

--- Sources/Day07/Day07.swift
+++ Sources/Day07/Day07.swift
@@ -6,17 +6,149 @@
 
 import AoCTools
 
-final class Day07: AOCDay {
-    let input: String
-    init(rawInput: String? = nil) {
-        self.input = rawInput ?? Self.rawInput
+private class Step {
+    let id: String
+    var prerequisites: [String]
+
+    init(id: String, prerequisites: [String] = []) {
+        self.id = id
+        self.prerequisites = prerequisites
     }
 
-    func part1() -> Int {
-        return 0
+    func isAvailable(done: String) -> Bool {
+        let done = Set(done.map { $0 })
+        let prereq = Set(prerequisites.map { Character($0) })
+        return prereq.isSubset(of: done)
+    }
+}
+
+private class Worker {
+    let id: String
+
+    private(set) var workingOn: String?
+    private(set) var finishedAt: Int?
+
+    init(id: String, workingOn: String? = nil, finishedAt: Int? = nil) {
+        self.id = id
+        self.workingOn = workingOn
+        self.finishedAt = finishedAt
+    }
+
+    func startWork(on step: String, finishedAt: Int) {
+        assert(workingOn == nil)
+        workingOn = step
+        self.finishedAt = finishedAt
+    }
+
+    func clear() {
+        workingOn = nil
+        finishedAt = nil
+    }
+}
+
+final class Day07: AOCDay {
+    private let steps: [String: Step]
+    private let workers: Int
+    private let baseDuration: Int
+
+    convenience init(rawInput: String?) {
+        self.init(rawInput: rawInput, workers: 5, baseDuration: 60)
+    }
+
+    init(rawInput: String? = nil, workers: Int, baseDuration: Int) {
+        let input = rawInput ?? Self.rawInput
+
+        var steps = [String: Step]()
+        for line in input.lines {
+            let parts = line.components(separatedBy: " ")
+            // "Step C must be finished before step A can begin."
+            let id = parts[7]
+            let prereq = parts[1]
+
+            if let step = steps[id] {
+                step.prerequisites.append(prereq)
+                step.prerequisites.sort(by: <)
+                steps[id] = step
+            } else {
+                steps[id] = Step(id: id, prerequisites: [prereq])
+            }
+        }
+        self.steps = steps
+        self.workers = workers
+        self.baseDuration = baseDuration
+    }
+
+    func part1() -> String {
+        let allSteps = Set(steps.flatMap { $0.value.prerequisites })
+        let start = allSteps.subtracting(steps.keys).sorted(by: <)
+
+        var available = start
+        var done = ""
+
+        while !available.isEmpty {
+            let id = available.removeFirst()
+            done.append(id)
+
+            let next = steps.values.filter { $0.isAvailable(done: done) }
+                .map { $0.id }
+                .filter { !done.contains($0) }
+
+            available = Set(available + next).sorted(by: <)
+        }
+        
+        return done
     }
 
     func part2() -> Int {
-        return 0
+        let allSteps = Set(steps.flatMap { $0.value.prerequisites })
+        let start = allSteps.subtracting(steps.keys).sorted(by: <)
+
+        var available = start
+        var done = ""
+        var second = 0
+
+        let workers = (0..<self.workers).map { Worker(id: "\($0)") }
+
+        while true {
+            var doneNow = [String]()
+            for worker in workers where worker.workingOn != nil {
+                if second >= worker.finishedAt! {
+                    doneNow.append(worker.workingOn!)
+                    worker.clear()
+                }
+            }
+
+            done += doneNow.sorted(by: <).joined(separator: "")
+            if done.count == steps.count + start.count {
+                break
+            }
+
+            let next = steps.values.filter { $0.isAvailable(done: done) }
+                .map { $0.id }
+                .filter { !done.contains($0) }
+                .filter { !workers.compactMap { $0.workingOn }.contains($0) }
+
+            available = Set(available + next).sorted(by: <)
+
+            for worker in workers where worker.workingOn == nil {
+                if available.isEmpty { break }
+                let id = available.removeFirst()
+                worker.startWork(on: id, finishedAt: second + duration(id))
+            }
+
+            if available.isEmpty {
+                let finished = workers.compactMap { $0.finishedAt }.min(by: <)
+                second = finished!
+            } else {
+                second += 1
+            }
+        }
+
+        return second
+    }
+
+    private func duration(_ id: String) -> Int {
+        let ch = Character(id).asciiValue!
+        return Int(ch - 64) + baseDuration
     }
 }
--- Tests/Day07Tests.swift
+++ Tests/Day07Tests.swift
@@ -3,12 +3,28 @@ import XCTest
 
 final class Day07Tests: XCTestCase {
     func testDay07_1() throws {
-        let day = Day07(rawInput: "foo")
-        XCTAssertEqual(day.part1(), 0)
+        let day = Day07(rawInput: """
+            Step C must be finished before step A can begin.
+            Step C must be finished before step F can begin.
+            Step A must be finished before step B can begin.
+            Step A must be finished before step D can begin.
+            Step B must be finished before step E can begin.
+            Step D must be finished before step E can begin.
+            Step F must be finished before step E can begin.
+            """)
+        XCTAssertEqual(day.part1(), "CABDFE")
     }
 
     func testDay07_2() throws {
-        let day = Day07(rawInput: "bar")
-        XCTAssertEqual(day.part2(), 0)
+        let day = Day07(rawInput: """
+            Step C must be finished before step A can begin.
+            Step C must be finished before step F can begin.
+            Step A must be finished before step B can begin.
+            Step A must be finished before step D can begin.
+            Step B must be finished before step E can begin.
+            Step D must be finished before step E can begin.
+            Step F must be finished before step E can begin.
+            """, workers: 2, baseDuration: 0)
+        XCTAssertEqual(day.part2(), 15)
     }
 }
