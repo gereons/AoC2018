ba59c326bfa4e3f155e6f318f03db61adb61dd6d
Gereon Steffens <gereon@steffens.org>
2022-10-22 11:43:29 +0200

    day 11 (brute-force)

--- Sources/Day11/Day11.swift
+++ Sources/Day11/Day11.swift
@@ -7,16 +7,82 @@
 import AoCTools
 
 final class Day11: AOCDay {
-    let input: String
+    let serial: Int
+    private var grid = [[Int]]()
+
     init(rawInput: String? = nil) {
-        self.input = rawInput ?? Self.rawInput
+        let input = rawInput ?? Self.rawInput
+        serial = Int(input)!
     }
 
-    func part1() -> Int {
-        return 0
+    func powerLevel(for x: Int, y: Int) -> Int {
+        let rackId = x + 10
+        let power = (rackId * y + serial) * rackId
+
+        let hundreds = (power / 100) % 10
+        return hundreds - 5
     }
 
-    func part2() -> Int {
-        return 0
+    func initGrid() {
+        guard grid.isEmpty else { return }
+
+        let line = [Int](repeating: 0, count: 301)
+        grid = [[Int]](repeating: line, count: 301)
+
+        for x in 1...300 {
+            for y in 1...300 {
+                grid[x][y] = powerLevel(for: x, y: y)
+            }
+        }
+    }
+
+    func part1() -> String {
+        initGrid()
+
+        var maxPower = Int.min
+        var maxPoint = Point.zero
+
+        for x in 2...299 {
+            for y in 2...299 {
+                let point = Point(x, y)
+                let neighbors = point.neighbors(adjacency: .all)
+                let powerSum = neighbors.reduce(0) { $0 + grid[$1.x][$1.y] } + grid[point.x][point.y]
+                if powerSum > maxPower {
+                    maxPower = powerSum
+                    maxPoint = Point(x-1, y-1)
+                }
+            }
+        }
+
+        return "\(maxPoint.x),\(maxPoint.y)"
+    }
+
+    func part2() -> String {
+        initGrid()
+
+        var maxPower = Int.min
+        var maxPoint = Point.zero
+        var maxSize = 0
+
+        for size in 1...300 {
+            for x in 1 ... 301 - size {
+                for y in 1 ... 301 - size {
+                    var powerSum = 0
+                    for nx in x ..< x + size {
+                        for ny in y ..< y + size {
+                            powerSum += grid[nx][ny]
+                        }
+                    }
+                    
+                    if powerSum > maxPower {
+                        maxPower = powerSum
+                        maxPoint = Point(x, y)
+                        maxSize = size
+                    }
+                }
+            }
+        }
+
+        return "\(maxPoint.x),\(maxPoint.y),\(maxSize)"
     }
 }
--- Tests/Day11Tests.swift
+++ Tests/Day11Tests.swift
@@ -1,14 +1,20 @@
 import XCTest
 @testable import AdventOfCode
+import AoCTools
 
 final class Day11Tests: XCTestCase {
     func testDay11_1() throws {
-        let day = Day11(rawInput: "foo")
-        XCTAssertEqual(day.part1(), 0)
+        XCTAssertEqual(Day11(rawInput: "8").powerLevel(for: 3, y: 5), 4)
+        XCTAssertEqual(Day11(rawInput: "57").powerLevel(for: 122, y: 79), -5)
+        XCTAssertEqual(Day11(rawInput: "39").powerLevel(for: 217, y: 196), 0)
+        XCTAssertEqual(Day11(rawInput: "71").powerLevel(for: 101,y: 153), 4)
+
+        XCTAssertEqual(Day11(rawInput: "18").part1(), "33,45")
+        XCTAssertEqual(Day11(rawInput: "42").part1(), "21,61")
     }
 
     func testDay11_2() throws {
-        let day = Day11(rawInput: "bar")
-        XCTAssertEqual(day.part2(), 0)
+        XCTAssertEqual(Day11(rawInput: "18").part2(), "90,269,16")
+        XCTAssertEqual(Day11(rawInput: "42").part2(), "232,251,12")
     }
 }
