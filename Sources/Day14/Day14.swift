559506250d876b5776ad7878b40b05e96f95ea12
Gereon Steffens <gereon@steffens.org>
2022-10-26 18:18:12 +0200

    day 14

--- Sources/Day14/Day14.swift
+++ Sources/Day14/Day14.swift
@@ -7,16 +7,75 @@
 import AoCTools
 
 final class Day14: AOCDay {
+    let recipes: Int
     let input: String
     init(rawInput: String? = nil) {
-        self.input = rawInput ?? Self.rawInput
+        input = rawInput ?? Self.rawInput
+        recipes = Int(input)!
     }
 
-    func part1() -> Int {
-        return 0
+    func part1() -> String {
+        var scores = [3, 7]
+        var elf1 = 0
+        var elf2 = 1
+
+        while true {
+            let sum = scores[elf1] + scores[elf2]
+            if sum > 9 {
+                scores.append(sum / 10)
+                scores.append(sum % 10)
+            } else {
+                scores.append(sum)
+            }
+            elf1 = (elf1 + 1 + scores[elf1]) % scores.count
+            elf2 = (elf2 + 1 + scores[elf2]) % scores.count
+            if scores.count >= self.recipes + 10 {
+                break
+            }
+        }
+
+        return scores[recipes..<recipes+10].map { String($0) }.joined(separator: "")
     }
 
     func part2() -> Int {
-        return 0
+        var scores = [3, 7]
+        var elf1 = 0
+        var elf2 = 1
+
+        let digits = input.map { Int(String($0))! }
+
+        while true {
+            let sum = scores[elf1] + scores[elf2]
+            if sum > 9 {
+                scores.append(sum / 10)
+                scores.append(sum % 10)
+            } else {
+                scores.append(sum)
+            }
+            elf1 = (elf1 + 1 + scores[elf1]) % scores.count
+            elf2 = (elf2 + 1 + scores[elf2]) % scores.count
+
+            let end = scores.suffix(digits.count)
+            if end == digits {
+                return scores.count - digits.count
+            }
+            let end2 = scores.suffix(digits.count + 1).dropLast()
+            if end2 == digits {
+                return scores.count - digits.count - 1
+            }
+        }
     }
+}
+
+func == (lhs: Array<Int>.SubSequence, rhs: [Int]) -> Bool {
+    if lhs.count != rhs.count { return false }
+
+    var i1 = lhs.makeIterator()
+    var i2 = rhs.makeIterator()
+    for _ in 0..<lhs.count {
+        if i1.next() != i2.next() {
+            return false
+        }
+    }
+    return true
 }
--- Tests/Day14Tests.swift
+++ Tests/Day14Tests.swift
@@ -3,12 +3,16 @@ import XCTest
 
 final class Day14Tests: XCTestCase {
     func testDay14_1() throws {
-        let day = Day14(rawInput: "foo")
-        XCTAssertEqual(day.part1(), 0)
+        XCTAssertEqual(Day14(rawInput: "9").part1(), "5158916779")
+        XCTAssertEqual(Day14(rawInput: "5").part1(), "0124515891")
+        XCTAssertEqual(Day14(rawInput: "18").part1(), "9251071085")
+        XCTAssertEqual(Day14(rawInput: "2018").part1(), "5941429882")
     }
 
     func testDay14_2() throws {
-        let day = Day14(rawInput: "bar")
-        XCTAssertEqual(day.part2(), 0)
+        XCTAssertEqual(Day14(rawInput: "51589").part2(), 9)
+        XCTAssertEqual(Day14(rawInput: "01245").part2(), 5)
+        XCTAssertEqual(Day14(rawInput: "92510").part2(), 18)
+        XCTAssertEqual(Day14(rawInput: "59414").part2(), 2018)
     }
 }
